20405947aaa5e868a388c4e666007cf2be875207
hessalti <hess.lee@altibase.com>
2022-08-14 10:59:45 +0900

    Update vip_change.sh

--- vip_change.sh
+++ vip_change.sh
@@ -4,18 +4,19 @@
 #Execute "sudo visudo" command in linux terminal
 #Append "hess ALL = NOPASSWD: /home1/hess/vip_change.sh" as a last line of the editing file
 
-if [ "$1" = "vip1" ] && [ "$2" = "up" ]; then
+#Modify network interface name and IP according to your environment
+
+if   [ "$1" = "1" ] && [ "$2" = "up" ]; then
     sudo ifconfig eth0:1 192.168.1.225 up
-fi
-
-if [ "$1" = "vip2" ] && [ "$2" = "up" ]; then
+elif [ "$1" = "2" ] && [ "$2" = "up" ]; then
     sudo ifconfig eth0:2 192.168.1.226 up
-fi
-
-if [ "$1" = "vip1" ] && [ "$2" = "down" ]; then
+elif [ "$1" = "1" ] && [ "$2" = "down" ]; then
     sudo ifconfig eth0:1 down
+elif [ "$1" = "2" ] && [ "$2" = "down" ]; then
+    sudo ifconfig eth0:2 down
+else
+    echo "Usage: vip_change.sh [1|2] [up|down]"
+    exit 1
 fi
 
-if [ "$1" = "vip2" ] && [ "$2" = "down" ]; then
-    sudo ifconfig eth0:2 down
-fi
+exit 0
