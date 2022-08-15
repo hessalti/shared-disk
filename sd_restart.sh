9a2a71d7fc7c577ba4bead54e994cb8896fcc6d3
hessalti <hess.lee@altibase.com>
2022-08-15 16:25:51 +0900

    Update sd_restart.sh

--- sd_restart.sh
+++ sd_restart.sh
@@ -5,3 +5,24 @@ server start
 (server start 실패한 경우) virtual IP delete $VIP
 (자동 Failover) 상황
 virtual IP delete $VIP
+
+
+########## working on 192.168.1.105:~
+#!/usr/bin/env bash
+
+if [ "$MYID" != "1" ] && [ "$MYID" != "2" ]; then
+    echo "Error: MYID environment variable is not set."
+    exit 1
+fi
+
+CONNECT_RESULT=$( ${ALTIBASE_HOME}/ZookeeperServer/bin/zkCli.sh -server 192.168.1.106:2181 quit | grep 'Session establishment complete' )
+
+if [ "x$CONNECT_RESULT" = "x" ]; then
+    #virtual IP delete $VIP
+else
+    server start
+
+fi
+
+echo "CONNECT_RESULT :" $CONNECT_RESULT
+exit 0
