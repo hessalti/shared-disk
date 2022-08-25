f6b69bebc75953a1363a3738c25ad22a60674aa9
hessalti <hess.lee@altibase.com>
2022-08-25 16:43:27 +0900

    Update vip_change.sh

--- vip_change.sh
+++ vip_change.sh
@@ -2,13 +2,17 @@
 
 #Preparation
 #Execute "sudo visudo" command in linux terminal
-#Append "hess ALL = NOPASSWD: /home1/hess/sd_mgmt/vip_change.sh" as a last line of the editing file
+#Append "hess ALL = NOPASSWD: /sbin/ifconfig" as a last line of the editing file
 
 if [ "$MY_ID" != "1" ] && [ "$MY_ID" != "2" ]; then
     echo "Error: MY_ID environment variable is not set."
     echo "Execute this command first : source ~/sd_mgmt/sd_set.env [1|2]"
     exit 1
 fi
+
+echo "\$MY_ID = $MY_ID"
+echo "\$MY_NETWORK_INTERFACE = $MY_NETWORK_INTERFACE"
+echo "\$MY_VIRTUAL_IP = $MY_VIRTUAL_IP"
 
 if   [ "$1" = "up" ]; then
     sudo ifconfig ${MY_NETWORK_INTERFACE} ${MY_VIRTUAL_IP} up
