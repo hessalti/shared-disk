cbf413497b0bead77df7929a1f9be710a0732846
hessalti <hess.lee@altibase.com>
2022-08-29 15:03:32 +0900

    Update sd_prepare_database.sh

--- sd_prepare_database.sh
+++ sd_prepare_database.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
+if [ "x$SD_MGMT" = "x" ]; then
+    echo "Error: SD_MGMT environment variable is not set."
+    echo "Find sd_set.env and execute this command first : source sd_set.env [1|2]"
+    exit 1
+fi
+
 if [ "$MY_ID" != "1" ] && [ "$MY_ID" != "2" ]; then
     echo "Error: MY_ID environment variable is not set."
-    echo "Execute this command first : source ~/sd_mgmt/sd_set.env [1|2]"
+    echo "Execute this command first : source ${SD_MGMT}/sd_set.env [1|2]"
     exit 1
 fi
 
 echo "########## Restart Altibase server : \$MY_ID = $MY_ID"
 echo "########## Add virtual IP : \$MY_ID = $MY_ID , \$MY_VIRTUAL_IP = $MY_VIRTUAL_IP"
-~/sd_mgmt/vip_change.sh up
-sleep 20
+${SD_MGMT}/vip_change.sh up
+sleep 10
 
 isql -silent -s 127.0.0.1 -u ${SYS_USER_ID} -p ${SYS_USER_PASSWD} -sysdba -noprompt << EOF
     startup process;
@@ -31,7 +37,7 @@ isql -silent -s 127.0.0.1 -u ${SYS_USER_ID} -p ${SYS_USER_PASSWD} -f ${ALTIBASE_
 isql -silent -s 127.0.0.1 -u ${SYS_USER_ID} -p ${SYS_USER_PASSWD} -f ${ALTIBASE_HOME}/packages/dbms_lock.sql
 isql -silent -s 127.0.0.1 -u ${SYS_USER_ID} -p ${SYS_USER_PASSWD} -f ${ALTIBASE_HOME}/packages/dbms_lock.plb
 
-isql -silent -s 127.0.0.1 -u ${SYS_USER_ID} -p ${SYS_USER_PASSWD} -f ~/sd_mgmt/base_schema.sql
+isql -silent -s 127.0.0.1 -u ${SYS_USER_ID} -p ${SYS_USER_PASSWD} -f ${SD_MGMT}/base_schema.sql
 isql -silent -s 127.0.0.1 -u ${SYS_USER_ID} -p ${SYS_USER_PASSWD} -noprompt << EOF
     EXEC DBMS_SHARD.CREATE_META();
     EXEC DBMS_SHARD.SET_LOCAL_NODE(${MY_ID}, 'NODE${MY_ID}', '${MY_VIRTUAL_IP}', ${ALTIBASE_PORT_NO}, '${MY_VIRTUAL_IP}', ${ALTIBASE_PORT_NO}, '${MY_VIRTUAL_IP}', ${ALTIBASE_REPLICATION_PORT_NO} );
