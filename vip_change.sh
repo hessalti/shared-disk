d3767648ec7ed13c1d5053b18fe9984aa98e6063
hessalti <hess.lee@altibase.com>
2022-08-29 14:57:29 +0900

    Update vip_change.sh

--- vip_change.sh
+++ vip_change.sh
@@ -4,9 +4,15 @@
 #Execute "sudo visudo" command in linux terminal
 #Append "hess ALL = NOPASSWD: /sbin/ifconfig" as a last line of the editing file
 
+if [ "x$SD_MGMT" = "x" ]; then
+    echo "Error: SD_MGMT environment variable is not set."
+    echo "Find sd_set.env and execute this command first : source sd_set.env [1|2]"
+    exit 1
+fi
+
 if [ "$MY_ID" != "1" ] && [ "$MY_ID" != "2" ]; then
     echo "Error: MY_ID environment variable is not set."
-    echo "Execute this command first : source ~/sd_mgmt/sd_set.env [1|2]"
+    echo "Execute this command first : source ${SD_MGMT}/sd_set.env [1|2]"
     exit 1
 fi
 
