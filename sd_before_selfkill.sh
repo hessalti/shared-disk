8bd95d49853e669e4ad511784953ee23d83df308
hessalti <hess.lee@altibase.com>
2022-08-30 17:29:10 +0900

    Update sd_before_selfkill.sh

--- sd_before_selfkill.sh
+++ sd_before_selfkill.sh
@@ -1,1 +1,15 @@
-virtual IP delete $VIP
+if [ "x$SD_MGMT" = "x" ]; then
+    echo "Error: SD_MGMT environment variable is not set."
+    echo "Find sd_set.env and execute this command first : source sd_set.env [1|2]"
+    exit 1
+fi
+
+if [ "$MY_ID" != "1" ] && [ "$MY_ID" != "2" ]; then
+    echo "########## Error: MY_ID environment variable is not set."
+    echo "########## Execute this command first : source ${SD_MGMT}/sd_set.env [1|2]"
+    exit 1
+fi
+
+${SD_MGMT}/vip_change.sh down
+
+exit 0
