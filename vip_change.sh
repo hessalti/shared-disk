28c2b52f8fb718484638a0bd357fc26779ac4cbf
hessalti <hess.lee@altibase.com>
2022-09-02 13:27:15 +0900

    Update vip_change.sh

--- vip_change.sh
+++ vip_change.sh
@@ -4,26 +4,26 @@
 #Execute "sudo visudo" command in linux terminal
 #Append "hess ALL = NOPASSWD: /sbin/ifconfig" as a last line of the editing file
 
-if [ "x$SD_MGMT" = "x" ]; then
-    echo "Error: SD_MGMT environment variable is not set."
+if [ "x$SHARED_DISK_MGMT" = "x" ]; then
+    echo "Error: SHARED_DISK_MGMT environment variable is not set."
     echo "Find sd_set.env and execute this command first : source sd_set.env [1|2]"
     exit 1
 fi
 
-if [ "$MY_ID" != "1" ] && [ "$MY_ID" != "2" ]; then
-    echo "Error: MY_ID environment variable is not set."
-    echo "Execute this command first : source ${SD_MGMT}/sd_set.env [1|2]"
+if [ "$SHARED_DISK_MY_ID" != "1" ] && [ "$SHARED_DISK_MY_ID" != "2" ]; then
+    echo "Error: SHARED_DISK_MY_ID environment variable is not set."
+    echo "Execute this command first : source ${SHARED_DISK_MGMT}/sd_set.env [1|2]"
     exit 1
 fi
 
-echo "\$MY_ID = $MY_ID"
-echo "\$MY_NETWORK_INTERFACE = $MY_NETWORK_INTERFACE"
-echo "\$MY_VIRTUAL_IP = $MY_VIRTUAL_IP"
+echo "\$SHARED_DISK_MY_ID = $SHARED_DISK_MY_ID"
+echo "\$SHARED_DISK_MY_NETWORK_INTERFACE = $SHARED_DISK_MY_NETWORK_INTERFACE"
+echo "\$SHARED_DISK_MY_VIRTUAL_IP = $SHARED_DISK_MY_VIRTUAL_IP"
 
 if   [ "$1" = "up" ]; then
-    sudo ifconfig ${MY_NETWORK_INTERFACE} ${MY_VIRTUAL_IP} up
+    sudo ifconfig ${SHARED_DISK_MY_NETWORK_INTERFACE} ${SHARED_DISK_MY_VIRTUAL_IP} up
 elif [ "$1" = "down" ]; then
-    sudo ifconfig ${MY_NETWORK_INTERFACE} down
+    sudo ifconfig ${SHARED_DISK_MY_NETWORK_INTERFACE} down
 else
     echo "Usage: vip_change.sh [up|down]"
     exit 1
