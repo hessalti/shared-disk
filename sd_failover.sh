187685a9ce3254daa5bba7cda2f5380dcf26b6c6
hessalti <hess.lee@altibase.com>
2022-09-02 13:17:38 +0900

    Update sd_failover.sh

--- sd_failover.sh
+++ sd_failover.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-if [ "x$SD_MGMT" = "x" ]; then
-    echo "Error: SD_MGMT environment variable is not set."
+if [ "x$SHARED_DISK_MGMT" = "x" ]; then
+    echo "Error: SHARED_DISK_MGMT environment variable is not set."
     echo "Find sd_set.env and execute this command first : source sd_set.env [1|2]"
     exit 1
 fi
 
-if [ "$MY_ID" != "1" ] && [ "$MY_ID" != "2" ]; then
-    echo "########## Error: MY_ID environment variable is not set."
-    echo "########## Execute this command first : source ${SD_MGMT}/sd_set.env [1|2]"
+if [ "$SHARED_DISK_MY_ID" != "1" ] && [ "$SHARED_DISK_MY_ID" != "2" ]; then
+    echo "########## Error: SHARED_DISK_MY_ID environment variable is not set."
+    echo "########## Execute this command first : source ${SHARED_DISK_MGMT}/sd_set.env [1|2]"
     exit 1
 fi
 
 
-if [ "$MY_ID" = "1" ]; then
-    source ${SD_MGMT}/sd_set.env 2
+if [ "$SHARED_DISK_MY_ID" = "1" ]; then
+    source ${SHARED_DISK_MGMT}/sd_set.env 2
 else
-    source ${SD_MGMT}/sd_set.env 1
+    source ${SHARED_DISK_MGMT}/sd_set.env 1
 fi
 
-echo "########## server start : \$MY_ID = $MY_ID"
+echo "########## server start : \$SHARED_DISK_MY_ID = $SHARED_DISK_MY_ID"
 SERVER_START_RESULT=$( server start )
 echo "########## SERVER_START_RESULT :" $SERVER_START_RESULT
 
@@ -28,16 +28,16 @@ echo "########## SERVER_START_OK :" $SERVER_START_OK
 
 if [ "x$SERVER_START_OK" = "x" ]; then
     echo "########## server start failure"
-    echo "########## Delete virtual IP : \$MY_ID = $MY_ID , \$MY_VIRTUAL_IP = $MY_VIRTUAL_IP"
-    ${SD_MGMT}/vip_change.sh down
+    echo "########## Delete virtual IP : \$SHARED_DISK_MY_ID = $SHARED_DISK_MY_ID , \$SHARED_DISK_MY_VIRTUAL_IP = $SHARED_DISK_MY_VIRTUAL_IP"
+    ${SHARED_DISK_SD_MGMT}/vip_change.sh down
     exit 1
 fi
 
-echo "########## server start success : \$MY_ID = $MY_ID"
-echo "########## Add virtual IP : \$MY_ID = $MY_ID , \$MY_VIRTUAL_IP = $MY_VIRTUAL_IP"
-${SD_MGMT}/vip_change.sh up
-sleep 10
-FAILBACK_RESULT=$(isql -s 127.0.0.1 -u ${SYS_USER_ID} -p ${SYS_USER_PASSWD} -silent -noprompt << 'EOF'
+echo "########## server start success : \$SHARED_DISK_MY_ID = $SHARED_DISK_MY_ID"
+echo "########## Add virtual IP : \$SHARED_DISK_MY_ID = $SHARED_DISK_MY_ID , \$SHARED_DISK_MY_VIRTUAL_IP = $SHARED_DISK_MY_VIRTUAL_IP"
+${SHARED_DISK_SD_MGMT}/vip_change.sh up
+sleep 4
+FAILBACK_RESULT=$(isql -s 127.0.0.1 -u ${SHARED_DISK_SYS_USER_ID} -p ${SHARED_DISK_SYS_USER_PASSWD} -silent -noprompt << 'EOF'
     ALTER DATABASE SHARD JOIN;
     ALTER DATABASE SHARD FAILBACK;
 EOF
@@ -51,8 +51,8 @@ if [ "x$FAILBACK_OK" = "x" ]; then
     echo "########## FAILBACK failure... server kill"
     server kill
 
-    echo "########## Delete virtual IP : \$MY_ID = $MY_ID , \$MY_VIRTUAL_IP = $MY_VIRTUAL_IP"
-    ${SD_MGMT}/vip_change.sh down
+    echo "########## Delete virtual IP : \$SHARED_DISK_MY_ID = $SHARED_DISK_MY_ID , \$SHARED_DISK_MY_VIRTUAL_IP = $SHARED_DISK_MY_VIRTUAL_IP"
+    ${SHARED_DISK_MGMT}/vip_change.sh down
     exit 1
 fi
 
