f9c0962686272b078299e54a3587a2f1f8c4ae9a
hessalti <hess.lee@altibase.com>
2022-09-02 13:11:00 +0900

    Update sd_before_selfkill.sh

--- sd_before_selfkill.sh
+++ sd_before_selfkill.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-if [ "x$SD_MGMT" = "x" ]; then
-    echo "Error: SD_MGMT environment variable is not set."
+if [ "x$SHARED_DISK_MGMT" = "x" ]; then
+    echo "Error: SHARED_DISK_MGMT environment variable is not set."
     echo "Find sd_set.env and execute this command first : source sd_set.env [1|2]"
     exit 1
 fi
 
-if [ "$MY_ID" != "1" ] && [ "$MY_ID" != "2" ]; then
-    echo "########## Error: MY_ID environment variable is not set."
-    echo "########## Execute this command first : source ${SD_MGMT}/sd_set.env [1|2]"
+if [ "$SHARED_DISK_MY_ID" != "1" ] && [ "$SHARED_DISK_MY_ID" != "2" ]; then
+    echo "########## Error: SHARED_DISK_MY_ID environment variable is not set."
+    echo "########## Execute this command first : source ${SHARED_DISK_MGMT}/sd_set.env [1|2]"
     exit 1
 fi
 
-${SD_MGMT}/vip_change.sh down
+${SHARED_DISK_MGMT}/vip_change.sh down
 
 exit 0
